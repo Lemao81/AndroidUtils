05ace968a47f361001d9f584c187454dc05fa977
Lemao81 <jr.berlin@web.de>
2018-01-21 11:36:58 +0100

    fix

--- utils/src/main/java/com/jueggs/utils/base/BaseActivityPresenter.kt
+++ utils/src/main/java/com/jueggs/utils/base/BaseActivityPresenter.kt
@@ -2,6 +2,6 @@ package com.jueggs.utils.base
 
 import android.content.Intent
 
-class BaseActivityPresenter<TView : BaseView> : BasePresenter<TView>() {
+abstract class BaseActivityPresenter<TView : BaseView> : BasePresenter<TView>() {
     open fun getExtras(intent: Intent) {}
 }
