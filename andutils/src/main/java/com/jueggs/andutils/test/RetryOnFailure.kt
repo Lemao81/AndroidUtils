33f060c61d29abcd67204d86a507d8038e8e1680
Lemao81 <jr.berlin@web.de>
2018-12-07 12:50:46 +0100

    Retry on failure test rule

---
+++ andutils/src/main/java/com/jueggs/andutils/test/RetryOnFailure.kt
@@ -0,0 +1,3 @@
+package com.jueggs.andutils.test
+
+annotation class RetryOnFailure(val count: Int)
---
+++ andutils/src/main/java/com/jueggs/andutils/test/RetryOnFailureRule.kt
@@ -0,0 +1,24 @@
+package com.jueggs.andutils.test
+
+import org.junit.rules.TestRule
+import org.junit.runner.Description
+import org.junit.runners.model.Statement
+
+class RetryOnFailureRule : TestRule {
+    override fun apply(base: Statement, description: Description): Statement {
+        return object : Statement() {
+            override fun evaluate() {
+                val retryCount = description.annotations.filterIsInstance<RetryOnFailure>().firstOrNull()?.count ?: 0
+
+                var throwable: Throwable? = null
+                repeat(retryCount + 1) {
+                    runCatching { base.evaluate() }
+                        .onSuccess { return }
+                        .onFailure { t -> throwable = t }
+                }
+
+                throwable?.let { throw it }
+            }
+        }
+    }
+}
