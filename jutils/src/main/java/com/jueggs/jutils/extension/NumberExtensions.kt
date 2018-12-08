dc369d1629bc39fab666367eec9f093ffd1ed8a2
Lemao81 <jr.berlin@web.de>
2018-12-08 12:09:23 +0100

    Number crop extensions

--- jutils/src/main/java/com/jueggs/jutils/extension/NumberExtensions.kt
+++ jutils/src/main/java/com/jueggs/jutils/extension/NumberExtensions.kt
@@ -1,3 +1,16 @@
 package com.jueggs.jutils.extension
 
+import kotlin.math.max
+import kotlin.math.min
+
 infix operator fun <T> Int.times(action: () -> T): List<T> = (0..this).map { action() }
+
+fun Double.cropTo(min: Double, max: Double) = min(max(min, this), max)
+fun Double.cropTo(min: Int, max: Double) = min(max(min.toDouble(), this), max)
+fun Double.cropTo(min: Double, max: Int) = min(max(min, this), max.toDouble())
+
+fun Float.cropTo(min: Float, max: Float) = min(max(min, this), max)
+fun Float.cropTo(min: Int, max: Float) = min(max(min.toFloat(), this), max)
+fun Float.cropTo(min: Float, max: Int) = min(max(min, this), max.toFloat())
+
+fun Int.cropTo(min: Int, max: Int) = min(max(min, this), max)
