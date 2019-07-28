926719af955373e70c225aeed126cb436bd48438
Lemao81 <jr.berlin@web.de>
2019-07-28 22:31:50 +0200

    add pluginmanagement to settings gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,15 @@
-include(":andutils", ":jutils", ":firebaseutils", ":andtestutils", ":resutils", ":customviewutils", ":jtestutils")
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+    }
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.namespace == "com.android") {
+                useModule("com.android.tools.build:gradle:${requested.version}")
+            }
+        }
+    }
+}
+
+include(":andutils", ":jutils", ":firebaseutils", ":andtestutils", ":resutils", ":customviewutils", ":jtestutils", ":rxutils")
